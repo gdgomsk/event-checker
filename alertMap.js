3c15272b8ba40ee1cfc838bb6af753b9196cb888
Jacob <you@example.com>
2013-02-10 13:13:08 +0700

    [master] Alert icon changes

--- alertMap.js
+++ alertMap.js
@@ -1,6 +1,24 @@
 var alertMap = [
     {
-        word: 'day',
+        word: 'hackathon',
+        handler: function() {
+            chrome.browserAction.setIcon({ path: 'alertIcon19.png' });
+        }
+    },
+    {
+        word: 'хакатон',
+        handler: function() {
+            chrome.browserAction.setIcon({ path: 'alertIcon19.png' });
+        }
+    },
+    {
+        word: 'devfest',
+        handler: function() {
+            chrome.browserAction.setIcon({ path: 'alertIcon19.png' });
+        }
+    },
+    {
+        word: 'конкурс',
         handler: function() {
             chrome.browserAction.setIcon({ path: 'alertIcon19.png' });
         }
